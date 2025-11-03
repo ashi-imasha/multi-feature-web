a8894da0cca95833443b2c816e3efe6900c4f72d
ashi-imasha <ashiniimasha30@gmail.com>
2025-11-03 22:21:54 +0530

    add contact page in feature-contact-page branch

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php $page = "Contact"; ?>
+  <!DOCTYPE html>
+  <html>
+  <head>
+ <title><?php echo $page; ?></title>
+ <style>
+ body { font-family: Arial; text-align: center; padding:50px; background: #fef9e7; }
+ h1 { color: #f39c12; }
+ </style>
+ </head>
+ <body>
+ <h1><?php echo $page; ?> Me</h1>
+ <p>Email: ashini@example.com</p>
+ <p>This is on the feature-contact-page branch!</p>
+ </body>
+ </html>
