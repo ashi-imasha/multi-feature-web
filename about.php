72c160c1d575ff82bd383fd6f4e2982c62c1199f
ashi-imasha <ashiniimasha30@gmail.com>
2025-11-03 22:18:01 +0530

    add about page in feature-about-page branch

---
+++ about.php
@@ -0,0 +1,15 @@
+<?php $page = "About"; ?>
+  <!DOCTYPE html>
+  <html>
+  <head>
+ <title><?php echo $page; ?></title>
+ <style>
+ body { font-family: Arial; text-align: center; padding:50px; background: #e8f4f8; }
+ h1 { color: #16a085; }
+ </style>
+  </head>
+  <body>
+ <h1><?php echo $page; ?> Me</h1>
+ <p>Hi, I’m Ashini! This page is on the feature-about-page branch.</p>
+  </body>
+  </html>
